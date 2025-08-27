0f7cee1ceef590e4d608eb231c826ebee5b4c701
Turzo02 <naimul198650@gmail.com>
2025-08-27 13:14:57 +0600

    Next Part added

--- script.js
+++ script.js
@@ -3,10 +3,25 @@
 const heartCount = document.getElementById("heartCount");
 heartCount.textContent = 0
 const hearts = document.querySelectorAll(".heartClick");
+
 hearts.forEach((heart) => {
     heart.addEventListener("click", () => {
         heartCount.textContent = parseInt(heartCount.textContent) + 1;
     })
 })
 
-//Call Buttons
+//🧪 Challenges Part (10 Marks)
+const copyCount = document.getElementById("copyCount");
+copyCount.textContent = 0
+
+
+
+
+let copyBtns = document.querySelectorAll(".copyBtn");
+copyBtns.forEach((copyBtn) => {
+    copyBtn.addEventListener("click", () => {
+          alert("Copied")
+        copyCount.textContent = parseInt(copyCount.textContent) + 1;
+    })
+})
+
