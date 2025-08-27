e738db58ffeca456513f37217b63419894a719de
Turzo02 <naimul198650@gmail.com>
2025-08-27 15:41:43 +0600

    Challege part added

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
-
-// Clicking on the 💗 heart icon 
+// Clicking on the 💗 heart icon
 const heartCount = document.getElementById("heartCount");
-heartCount.textContent = 0
+heartCount.textContent = 0;
 const hearts = document.querySelectorAll(".heartClick");
 
 hearts.forEach((heart) => {
-    heart.addEventListener("click", () => {
-        heartCount.textContent = parseInt(heartCount.textContent) + 1;
-    })
-})
+  heart.addEventListener("click", () => {
+    heartCount.textContent = parseInt(heartCount.textContent) + 1;
+  });
+});
 
 //🧪 Challenges Part (10 Marks)
 const copyCount = document.getElementById("copyCount");
-copyCount.textContent = 0
+copyCount.textContent = 0;
+const copyBtns = document.querySelectorAll(".copyBtn");
 
-
-
-
-let copyBtns = document.querySelectorAll(".copyBtn");
 copyBtns.forEach((copyBtn) => {
-    copyBtn.addEventListener("click", () => {
-          alert("Copied")
-        copyCount.textContent = parseInt(copyCount.textContent) + 1;
-    })
-})
+  copyBtn.addEventListener("click", () => {
+    const card =copyBtn.closest(".card")
+    const textToCopy = card.querySelector(".hotlineNumber").textContent;
+    console.log(textToCopy);
+    navigator.clipboard.writeText(textToCopy).then(() => {
+      alert("Hotline Number Copied❗");
+      copyCount.textContent = parseInt(copyCount.textContent) + 1;
+    });
+  });
+});
 
+//Call Buttons
