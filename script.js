30347c57f6590144f44d13adecb2c70e01b3945c
Turzo02 <naimul198650@gmail.com>
2025-08-27 12:36:10 +0600

    JS section1 added

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log("Assignment 5");
+
+// Clicking on the 💗 heart icon 
+const heartCount = document.getElementById("heartCount");
+heartCount.textContent = 0
+const hearts = document.querySelectorAll(".heartClick");
+hearts.forEach((heart) => {
+    heart.addEventListener("click", () => {
+        heartCount.textContent = parseInt(heartCount.textContent) + 1;
+    })
+})
+
+//Call Buttons
